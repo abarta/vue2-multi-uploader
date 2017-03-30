6939c03d99916f9e2fc936e8413aad3fe2e79541
A <a@a.com>
2017-03-31 00:57:46 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,80 @@
+# vue2-multi-uploader
+
+> Drag and drop multiple file uploader with Vue.js v2 and Axios
+
+**Current Version**
+1.0.0
+
+**Install**
+
+``` bash
+npm install --save vue2-multi-uploader
+```
+Import Component
+
+    import MultipleFileUploader from 'vue2-multi-uploader'
+    export default {
+        components: {
+            MultipleFileUploader
+        }
+    }
+
+**Props**
+
+| Prop name        | Type           | Required  |
+| ------------- |:-------------:| -----:|
+| postURL      | String |  yes
+| successMessagePath      | String    |   yes |
+| errorMessagePath | String     |    yes |
+| minItems | Number     |    no |
+
+postURL
+Set your POST url in the postURL prop.
+
+JSON Responses
+Set JSON success and error response messages to your custom JSON responses via props:
+`successMessagePath`,
+`errorMessagePath`
+
+Required props example template
+``` bash
+<multiple-file-uploader postURL="http://.." successMessagePath="" errorMessagePath=""></multiple-file-uploader>
+```
+
+Minimum items to upload (optional)
+You can modify the minimum number of files required for the upload to take place by changing the value of minItems. Default if 2. 
+If you set this prop to 1, you can use this uploader for a single file upload, like so:
+``` bash
+<multiple-file-uploader postURL="http://.." successMessagePath="" errorMessagePath="" minItems="1"></multiple-file-uploader>
+```
+**Dependencies**
+Axios ^v0.15.3
+
+**CSS**
+Bootstrap CSS is used in demo but not required for the component.
+CSS style is not scoped so it's easy for you to overwrite your own style.
+
+**LICENSE**
+---
+The MIT License (MIT)
+
+Copyright (c) 2017 Andrei Barta
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
---
+++ src/MultipleFileUploader.vue
@@ -0,0 +1,226 @@
+<template>
+    <div class="uploadBox">
+        <h3>Add files</h3>
+        <form role="form" method="post" enctype="multipart/form-data" @submit.prevent="onSubmit">
+            <div class="uploadBoxMain" v-if="itemsAdded < minItems">
+                <div class="form-group">
+                    <div class="dropArea" @ondragover="onChange">
+                        Drop multiple files here.
+                        <input type="file" id="items" name="items[]" required multiple @change="onChange">
+                        <p class="help-block">Space for your instructions</p>
+                    </div>
+                </div>
+            </div>
+            <div class="uploadBoxMain" v-else>
+                <p><strong>Names</strong></p>
+                <ol>
+                    <li v-for="name in itemsNames">{{name}}</li>
+                </ol>
+                <p><strong>Sizes</strong></p>
+                <ol>
+                    <li v-for="size in itemsSizes">{{size}}</li>
+                </ol>
+                <p><strong>Total files:</strong> {{itemsAdded}}</p>
+                <p><strong>Total upload size:</strong> {{itemsTotalSize}}</p>
+                <button @click="removeItems">Remove files</button>
+                <!-- Loader -->
+                <div class="loader" v-if="isLoaderVisible">
+                    <div class="loaderImg"></div>
+                </div>
+                <!-- End Loader -->
+            </div>
+            <div>
+                <button type="submit" class="btn btn-primary btn-black btn-round" :disabled="itemsAdded < minItems">Upload</button>
+                <button type="button" class="btn btn-default btn-round" @click="removeItems">Cancel</button>
+            </div>
+            <br>
+            <div class="successMsg" v-if="successMsg !== ''">{{successMsg}}</div>
+            <div class="errorMsg" v-if="errorMsg !== ''">An error has occured:<br>{{errorMsg}}</div>
+        </form>
+    </div>
+</template>
+
+<script>
+require('es6-promise').polyfill();
+import axios from 'axios';
+
+component: {axios}
+export default {
+    props: {
+        postURL: {
+            type: String,
+            required: true
+        },
+        minItems: {
+            type: Number,
+            default: 2
+        },
+        successMessagePath: {
+            type: String,
+            required: true
+        },
+        errorMessagePath: {
+            type: String,
+            required: true
+        }
+    },
+
+    /*
+     * The component's data.
+     */
+    data() {
+        return {
+            items: [],
+            itemsAdded: '',
+            itemsNames: [],
+            itemsSizes: [],
+            itemsTotalSize: '',
+            formData: '',
+            successMsg: '',
+            errorMsg: '',
+            isLoaderVisible: false,
+        }
+    },
+
+    methods: {
+        // http://scratch99.com/web-development/javascript/convert-bytes-to-mb-kb/
+        bytesToSize(bytes) {
+            const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+            if (bytes == 0) return 'n/a';
+            let i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+            if (i == 0) return bytes + ' ' + sizes[i];
+            return (bytes / Math.pow(1024, i)).toFixed(2) + ' ' + sizes[i];
+        },
+
+        onChange(e) {
+            this.successMsg = '';
+            this.errorMsg = '';
+            this.formData = new FormData();
+            let files = e.target.files || e.dataTransfer.files;
+            this.itemsAdded = files.length;
+            let fileSizes = 0;
+            for (let x in files) {
+                if (!isNaN(x)) {
+                    this.items = e.target.files[x] || e.dataTransfer.files[x];
+                    this.itemsNames[x] = files[x].name;
+                    this.itemsSizes[x] = this.bytesToSize(files[x].size);
+                    fileSizes += files[x].size;
+                    this.formData.append('items[]', this.items);
+                }
+            }
+            this.itemsTotalSize = this.bytesToSize(fileSizes);
+        },
+
+        removeItems() {
+            this.items = '';
+            this.itemsAdded = '';
+            this.itemsNames = [];
+            this.itemsSizes = [];
+            this.itemsTotalSize = '';
+        },
+
+        onSubmit() {
+            this.isLoaderVisible = true;
+            axios.post(this.postURL, this.formData)
+                .then((response) => {
+                    this.isLoaderVisible = false;
+                    // Show success message
+                    this.successMsg = response+"."+this.successMessagePath;
+                    this.removeItems();
+                })
+                .catch((error) => {
+                    this.isLoaderVisible = false;
+                    this.errorMsg = error+"."+this.errorMessagePath;
+                    this.removeItems();
+                });
+        },
+    }
+}
+</script>
+
+<style>
+.uploadBox {
+    position: relative;
+    background: #eee;
+    padding: 0 1em 1em 1em;
+    margin: 1em;
+}
+
+.uploadBox h3 {
+    padding-top: 1em;
+}
+
+.uploadBox .uploadBoxMain {
+    position: relative;
+    margin-bottom: 1em;
+    margin-right: 1em;
+}
+
+/* Drag and drop */
+.uploadBox .dropArea {
+    position: relative;
+    width: 100%;
+    height: 300px;
+    border: 5px dashed #00ADCE;
+    text-align: center;
+    font-size: 2em;
+    padding-top: 80px;
+}
+
+.uploadBox .dropArea input {
+    position: absolute;
+    cursor: pointer;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    opacity: 0;
+}
+/* End drag and drop */
+
+/* Loader */
+.uploadBox .loader {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #fff;
+    opacity: 0.9;
+}
+
+.uploadBox .loaderImg {
+    border: 9px solid #eee;
+    border-top: 9px solid #00ADCE;
+    border-radius: 50%;
+    width: 70px;
+    height: 70px;
+    animation: spin 1s linear infinite;
+}
+
+@keyframes spin {
+    0% {
+        transform: rotate(0deg);
+    }
+    100% {
+        transform: rotate(360deg);
+    }
+}
+/* End Loader */
+
+.uploadBox .errorMsg {
+    font-size: 2em;
+    color: #a94442;
+}
+
+.uploadBox .successMsg {
+    font-size: 2em;
+    color: #3c763d;
+}
+</style>
+
