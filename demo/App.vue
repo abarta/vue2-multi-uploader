a7b6af745d770fef043eff6e4968fa587f4db1a1
A <a@a.com>
2017-07-24 22:28:16 +0300

    added demo files

---
+++ demo/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="app">
+        <multiple-file-uploader postURL="" successMessagePath="" errorMessagePath=""></multiple-file-uploader>
+    </div>
+</template>
+
+<script>
+    import MultipleFileUploader from './../src/MultipleFileUploader.vue'
+
+    export default {
+        components: {
+            MultipleFileUploader
+        }
+    }
+</script>
